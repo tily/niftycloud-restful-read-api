fe240e895d5347ccb8f99318e4beeebdfcd35b6a
tily <tily05@gmail.com>
2014-06-26 03:10:12 +0900

    fix bug

--- lib/niftycloud-restful-read-api.rb
+++ lib/niftycloud-restful-read-api.rb
@@ -10,7 +10,7 @@ class NiftycloudRestfulReadApi < Sinatra::Base
         @api = NIFTY::Cloud::Base.new(
           :access_key => options[:access_key_id],
           :secret_key => options[:secret_access_key],
-          :endpoint => "{options[:region]}.cp.cloud.nifty.com",
+          :server => "{options[:region]}.cp.cloud.nifty.com",
           :path => '/api'
         )
       end
